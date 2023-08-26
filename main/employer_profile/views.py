a8836836eeb7a1ed72751424e6538306004ce400
unknown <hayhuhin.new@gmail.com>
2023-08-26 09:53:43 +0300

    no changes

--- main/employer_profile/views.py
+++ main/employer_profile/views.py
@@ -26,24 +26,25 @@ def html_rank_policy(user,rank,calculations):
 
 
 def employer(request):
-    post = 'vala'
-    employers_rank(post)
     curr_path = Path.cwd()
 
+ #graph creating class
     instance_of_graph_creator = graph_creator()
+ 
+
 
     # contribution pie graph 
     path = str(curr_path) +"/employer_profile/static/employer/images/pie.png"
     values = [20, 50, 37, 18]
-    names = ['sales 1', 'sales 2', 'sales 3', 'sales 4']
-    revenue_pie_chart = instance_of_graph_creator.pie_graph(names=names,values=values,to_html=False,path=path)
+    names = ['week 1', 'week 2', 'week 3', 'week 4']
+    revenue_pie_chart = instance_of_graph_creator.pie_graph(names=names,values=values)
 
 
     # contribution pie graph 
     path = str(curr_path)+"/employer_profile/static/employer/images/donut.png"
-    values=[20, 50, 37, 18]
-    names= ['sales 1', 'sales 2', 'sales 3', 'sales 4']
-    contrib_donut_chart = instance_of_graph_creator.donut_graph(values=values,names= names,to_html=False,path=path)
+    values=[20, 50, 37, 18,22]
+    names= ['day 1', 'day 2', 'day 3', 'day 4','day 5']
+    contrib_donut_chart = instance_of_graph_creator.donut_graph(values=values,names= names)
 
 
 
