6e33485c029b889bd4a265515299114da920590a
hayhuhin <hayhuhin.new@gmail.com>
2023-09-19 17:39:28 +0300

    alert messages added

--- main/static/script.js
+++ main/static/script.js
@@ -1,5 +1,32 @@
 
+
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
+
+
+
 //department tasks post handling ajax functions
+
 
 function department_task_complete_ajax (e) {
     var member_id = $(this).attr('member_id');
@@ -95,7 +122,6 @@ function add_task_ajax (e)  {
 function complete_task_ajax (e)  {
     var member_id = $(this).attr('member_id');
     e.preventDefault();
-
     $.ajax({
         type:'POST',
         url: "/tasks",
@@ -103,16 +129,24 @@ function complete_task_ajax (e)  {
             csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
             name:'complete_task_form',
             id:member_id,
+            success: function() { 
+                setTimeout(function(){
+                    location.reload(); 
+                }, 1500);
+                $('#liveAlertBtn').html(appendAlert('Nice, you triggered this alert message!', 'success')); 
+         
+    },},
 
-        },
-        success:function(data){
-            $('#success').html("as")
-            location.reload(true)
-            
-        }
-    })
+})
+
 
 };
+
+
+
+
+
+
 
 function on_progress_task_ajax (e)  {
     var member_id = $(this).attr('member_id');
