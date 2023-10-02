71727a7a543f0b476d468570161b983fa1b7327a
hayhuhin <hayhuhin.new@gmail.com>
2023-10-02 11:34:59 +0300

    ADDED:the dashboard page changed and in maintenance for now

--- main/dashboard/views.py
+++ main/dashboard/views.py
@@ -19,19 +19,19 @@ def dashboard(request):
 
     #all of this code below is only for testing and modifying the frontend
     df = pd.DataFrame(dict(
-    group = ["A", "B", "C", "D", "E"],
-    value = [14, 12, 8, 10, 16]))
+    months = ["1", "2", "3", "4", "5","6","7","8","9","10","11","12"],
+    income = [14, 12, 8, 10, 16,14, 12, 8, 10, 16,15,1]))
     print(df)
 
-    graph_fig = px.bar(df, x = 'group', y = 'value')
+    graph_fig = px.bar(df, x = 'months', y = 'income')
     graph_chart = graph_fig.to_html()
 
     pie_fig = px.pie(values = [20, 50, 37, 18],
         names = ['G1', 'G2', 'G3', 'G4'])
     pie_chart = pie_fig.to_html()
 
-    line_fig = px.line(x = [1, 2, 3, 4, 5, 6, 7],
-        y = [10, 15, 25, 18, 43, 30, 65])
+    line_fig = px.line(x = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12],
+        y = [10, 15, 25, 18, 43, 30, 65,25,35,67,1,89])
     line_chart = line_fig.to_html()
     
     line_2_fig = px.line(x = [1, 2, 3, 4, 5, 6, 7],
